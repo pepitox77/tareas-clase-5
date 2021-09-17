3506019316e036a5be0b7f7aef67b32e1d96d498
pepitox <lavashuriken777@gmail.com>
2021-09-17 15:51:20 -0300

    Varios cambios
    Las tareas requerian que las funciones usen parametros y tengan nombres mas adecuados, además, ahora funcionan con cualquier cantidad de numeros agregados en el HTML

--- tarea 2/tarea2.js
+++ tarea 2/tarea2.js
@@ -7,10 +7,10 @@ datosUsuario.onclick = function () {
     apellidoUsuario = document.querySelector('#apellido-usuario').value
 
     if (nombreUsuario != "") {
-        document.querySelector('#resultado0').innerText = `Tu nombre es: ${nombreUsuario}`
-        document.querySelector('#resultado1').innerText = `Tu segundo nombre es: ${segundoNombreUsuario}`
-        document.querySelector('#resultado2').innerText = `Tu apellido es: ${apellidoUsuario}`
-        document.querySelector('#resultado3').innerText = `Tu edad es: ${edadUsuario}`
+        document.querySelector('#primer-nombre').innerText = `Tu nombre es: ${nombreUsuario}`
+        document.querySelector('#segundo-nombre').innerText = `Tu segundo nombre es: ${segundoNombreUsuario}`
+        document.querySelector('#apellido').innerText = `Tu apellido es: ${apellidoUsuario}`
+        document.querySelector('#edad').innerText = `Tu edad es: ${edadUsuario}`
         document.querySelector('#titulo').innerText = `Bienvenido, ${nombreUsuario}!`
     } return false
 }
--- tarea 3/tarea3.js
+++ tarea 3/tarea3.js
@@ -1,59 +1,48 @@
-function sumarVideos(num1, num2, num3, num4, num5) {
-    return num1 + num2 + num3 + num4 + num5
-    }
-    
-function sumar(num1, num2) {
-        return num1 + num2
+function calcularSegundosYMinutosExcedientes(Segundos_o_minutos, Minutos_o_horas) {
+    if (Segundos_o_minutos > 60) {
+        for (i = Segundos_o_minutos; i > 60; i = i - 60) {
+            Minutos_o_horas += 1
+            Segundos_o_minutos = (Segundos_o_minutos - 60)
         }
-
-function calcularSegundosYMinutos(num1, num2) {
-    if (num1 > 60) {
-        for (i = num1; i > 60; i = i - 60){ 
-            num2 += 1
-        num1 = (num1 - 60)
-    }}
-    return num2 }
-
-function calcularSegundosYMinutosRestantes(num) {
-    if (num > 60) {
-        for (i = num; i > 60; i = i - 60)
-            num -= 60
     }
-    return num
+    return Minutos_o_horas
+}
+
+function calcularSegundosYMinutosRestantes(Segundos_o_minutos) {
+    if (Segundos_o_minutos > 60) {
+        for (i = Segundos_o_minutos; i > 60; i = i - 60)
+            Segundos_o_minutos -= 60
+    }
+    return Segundos_o_minutos
+}
+
+function devolverUnidadDeTiempoTotal(unidadDeTiempo) {
+    let sumador = 0
+    for (i = 0; i < unidadDeTiempo.length; ++i) {
+        sumador = sumador + Number(unidadDeTiempo[i].value);
+    }
+    return sumador
 }
 
 
 botonIngresar = document.querySelector('#ingresar')
 
-
-let calcularVideos = botonIngresar.onclick = function(){ 
-
-segundosTotales = sumarVideos(Number(document.querySelector('#segundos-video0').value),
-Number(document.querySelector('#segundos-video1').value),
-Number(document.querySelector('#segundos-video2').value),
-Number(document.querySelector('#segundos-video3').value),
-Number(document.querySelector('#segundos-video4').value))
-
-minutosTotales = sumarVideos(Number(document.querySelector('#minutos-video0').value),
-Number(document.querySelector('#minutos-video1').value),
-Number(document.querySelector('#minutos-video2').value),
-Number(document.querySelector('#minutos-video3').value),
-Number(document.querySelector('#minutos-video4').value))
-
-horasTotales = sumarVideos(Number(document.querySelector('#hora-video0').value),
-Number(document.querySelector('#hora-video1').value),
-Number(document.querySelector('#hora-video2').value),
-Number(document.querySelector('#hora-video3').value),
-Number(document.querySelector('#hora-video4').value))
+let segundos = document.querySelectorAll(".segundos")
+let minutos = document.querySelectorAll(".minutos")
+let horas = document.querySelectorAll(".horas")
 
 
 
-
-let MINUTOS_CON_SEGUNDOS_AGREGADOS = calcularSegundosYMinutos(segundosTotales, minutosTotales)
-let SEGUNDOS_RESTANTES = calcularSegundosYMinutosRestantes(segundosTotales)
-let MINUTOS_RESTANTES = calcularSegundosYMinutosRestantes(MINUTOS_CON_SEGUNDOS_AGREGADOS)
-let HORAS_RESTANTES = calcularSegundosYMinutos(MINUTOS_CON_SEGUNDOS_AGREGADOS, horasTotales)
-
-if (segundosTotales || minutosTotales || horasTotales != ""){ 
-document.querySelector("#resultado").innerText = ` La suma total del tiempo de los videos es: ${HORAS_RESTANTES} horas ${MINUTOS_RESTANTES} minutos ${SEGUNDOS_RESTANTES} segundos. `
-}}
+botonIngresar.onclick = function () {
+    segundosTotales = devolverUnidadDeTiempoTotal(segundos)
+    minutosTotales = devolverUnidadDeTiempoTotal(minutos)
+    horasTotales = devolverUnidadDeTiempoTotal(horas)
+    minutosConSegundosAgregados = calcularSegundosYMinutosExcedientes(segundosTotales, minutosTotales)
+    horasConMinutosAgregados = calcularSegundosYMinutosExcedientes(minutosConSegundosAgregados, horasTotales)
+    if (segundosTotales === 0 && minutosTotales === 0 && horasTotales === 0) {
+        document.querySelector('#resultado').innerText = "No ingresaste nada!!"
+    }
+    else {
+        document.querySelector('#resultado').innerText = `El tiempo total de todos los videos es: ${horasConMinutosAgregados} horas, ${calcularSegundosYMinutosRestantes(minutosConSegundosAgregados)} minutos y ${calcularSegundosYMinutosRestantes(segundosTotales)} segundos.`
+    }
+}
